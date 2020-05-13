a47e7af71f7141381ad1a8f7e21f2848e93998f1
Anbu Selvam Mani <anman57@inter-ikea.com>
2020-05-13 23:51:34 +0200

    runtime file v1

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+# main run
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!\n'
+
+
+if __name__ == '__main__':
+    app.run()
