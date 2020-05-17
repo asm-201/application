3696430c127c18bcc584896311d5d313032086ce
Anbu Selvam Mani <anman57@inter-ikea.com>
2020-05-17 22:55:46 +0200

    Baseline template + Added two htmls admin-Accounts, admin-Address

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world. First run!\n'
+    return 'Hello world. First run. change to dynamism!\n'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
